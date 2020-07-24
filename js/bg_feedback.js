b2e8b1641bef0748b1e1246e5c3e03909977e4f3
Dmitry <it_proger@mail.ru>
2020-07-24 22:17:46 +0300

    kolotilshikov_v4

--- js/bg_feedback.js
+++ js/bg_feedback.js
@@ -1,14 +1,14 @@
 VANTA.WAVES({
     el: "#wave_id",
-    mouseControls: true,
-    touchControls: true,
+    mouseControls: false,
+    touchControls: false,
     minHeight: 200.00,
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
     color: 0x141414,
     shininess: 80.00,
-    waveHeight: 9.50,
-    waveSpeed: 0.7,
-    zoom: 0.7
+    waveHeight: 7.50,
+    waveSpeed: 0.6,
+    zoom: 0.65
   })
